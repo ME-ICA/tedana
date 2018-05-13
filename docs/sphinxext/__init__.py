cbb828a16934df4774911b72f144ff116a828f54
Elizabeth DuPre <emd222@cornell.edu>
2018-05-12 22:06:35 -0400

    Add linkcode sphinxext

---
+++ docs/sphinxext/__init__.py
@@ -0,0 +1,11 @@
+# emacs: -*- mode: python-mode; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
+# ex: set sts=4 ts=4 sw=4 et:
+
+from .github_link import (
+    make_linkcode_resolve,
+)
+
+
+__all__ = [
+    'make_linkcode_resolve',
+    ]
