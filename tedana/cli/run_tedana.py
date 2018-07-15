f448954841e276f64585b3cc7e42e15be2b4177a
Taylor Salo <tsalo006@fiu.edu>
2018-07-15 19:53:04 -0400

    Update t2smap CLI.

--- tedana/cli/run_tedana.py
+++ tedana/cli/run_tedana.py
@@ -8,7 +8,8 @@ import argparse
 from tedana import workflows
 
 import logging
-logging.basicConfig(format='[%(levelname)s]: ++ %(message)s', level=logging.INFO)
+logging.basicConfig(format='[%(levelname)s]: ++ %(message)s',
+                    level=logging.INFO)
 
 
 def is_valid_file(parser, arg):
@@ -178,16 +179,6 @@ def main(argv=None):
     elif options.quiet:
         logging.getLogger().setLevel(logging.WARNING)
     workflows.tedana(**vars(options))
-
-
-def run_t2smap(argv=None):
-    """T2smap entry point"""
-    options = get_parser().parse_args(argv)
-    if options.debug and not options.quiet:
-        logging.getLogger().setLevel(logging.DEBUG)
-    elif options.quiet:
-        logging.getLogger().setLevel(logging.WARNING)
-    workflows.t2smap(**vars(options))
 
 
 if __name__ == '__main__':
