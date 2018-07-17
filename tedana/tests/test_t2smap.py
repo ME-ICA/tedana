7b96912fff4f71d8601ea9f325411bd1e72babc2
Taylor Salo <tsalo006@fiu.edu>
2018-07-16 20:38:10 -0400

    Add t2smap workflow test.

--- tedana/tests/test_t2smap.py
+++ tedana/tests/test_t2smap.py
@@ -6,20 +6,20 @@ import os.path
 import numpy as np
 import nibabel as nb
 from pathlib import Path
-from tedana.cli import run_tedana
+from tedana.cli import run_t2smap
 from tedana import workflows
 
 
-def test_basic_tedana():
+def test_basic_t2smap():
     """
-    A very simple test, to confirm that tedana creates output
+    A very simple test, to confirm that t2smap creates output
     files.
     """
-    parser = run_tedana.get_parser()
+    parser = run_t2smap.get_parser()
     options = parser.parse_args(['-d', '/home/neuro/data/zcat_ffd.nii.gz',
                                  '-e', '14.5', '38.5', '62.5'])
-    workflows.tedana(**vars(options))
-    assert os.path.isfile('comp_table.txt')
+    workflows.t2smap(**vars(options))
+    assert os.path.isfile('ts_OC.nii')
 
 
 def compare_nifti(fn, test_dir, res_dir):
@@ -39,27 +39,9 @@ def test_outputs():
     nifti_test_list = [
         't2sv.nii',
         's0v.nii',
-        't2ss.nii',
-        's0vs.nii',
         't2svG.nii',
         's0vG.nii',
-        'T1gs.nii',
-        'tsoc_orig.nii',
-        'tsoc_nogs.nii',
-        'veins_l0.nii',
-        'veins_l1.nii',
         'ts_OC.nii',
-        'hik_ts_OC.nii',
-        'midk_ts_OC.nii',
-        'lowk_ts_OC.nii',
-        'dn_ts_OC.nii',
-        'betas_OC.nii',
-        'betas_hik_OC.nii',
-        'feats_OC2.nii',
-        'betas_hik_OC_T1c.nii',
-        'dn_ts_OC_T1c.nii',
-        'hik_ts_OC_T1c.nii',
-        'sphis_hik.nii'
     ]
     for fn in nifti_test_list:
         compare_nifti(fn, Path('/home/neuro/data/TED/'),
