446961786fc7a8549e7ebe8f466fe832a1815b40
Taylor Salo <tsalo006@fiu.edu>
2018-07-16 20:50:02 -0400

    Fix t2smap tests.

--- tedana/tests/test_t2smap.py
+++ tedana/tests/test_t2smap.py
@@ -17,9 +17,38 @@ def test_basic_t2smap():
     """
     parser = run_t2smap.get_parser()
     options = parser.parse_args(['-d', '/home/neuro/data/zcat_ffd.nii.gz',
-                                 '-e', '14.5', '38.5', '62.5'])
+                                 '-e', '14.5', '38.5', '62.5',
+                                 '--label', 't2smap0'])
     workflows.t2smap(**vars(options))
-    assert os.path.isfile('ts_OC.nii')
+    assert os.path.isfile('/home/neuro/code/TED.zcat_ffd.t2smap0/ts_OC.nii')
+
+
+def test_basic_t2smap2():
+    """
+    A very simple test, to confirm that t2smap creates output
+    files when fitmode is set to ts.
+    """
+    parser = run_t2smap.get_parser()
+    options = parser.parse_args(['-d', '/home/neuro/data/zcat_ffd.nii.gz',
+                                 '-e', '14.5', '38.5', '62.5',
+                                 '--fitmode', 'ts',
+                                 '--label', 't2smap1'])
+    workflows.t2smap(**vars(options))
+    assert os.path.isfile('/home/neuro/code/TED.zcat_ffd.t2smap0/ts_OC.nii')
+
+
+def test_basic_t2smap3():
+    """
+    A very simple test, to confirm that t2smap creates output
+    files when combmode is set to 'ste'.
+    """
+    parser = run_t2smap.get_parser()
+    options = parser.parse_args(['-d', '/home/neuro/data/zcat_ffd.nii.gz',
+                                 '-e', '14.5', '38.5', '62.5',
+                                 '--combmode', 'ste',
+                                 '--label', 't2smap2'])
+    workflows.t2smap(**vars(options))
+    assert os.path.isfile('/home/neuro/code/TED.zcat_ffd.t2smap2/ts_OC.nii')
 
 
 def compare_nifti(fn, test_dir, res_dir):
@@ -45,4 +74,4 @@ def test_outputs():
     ]
     for fn in nifti_test_list:
         compare_nifti(fn, Path('/home/neuro/data/TED/'),
-                      Path('/home/neuro/code/TED.zcat_ffd/'))
+                      Path('/home/neuro/code/TED.zcat_ffd.t2smap0/'))
