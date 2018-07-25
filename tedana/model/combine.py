ea93548e4e9d759390ea6bc3968cf92f3da3c2b5
Taylor Salo <tsalo006@fiu.edu>
2018-07-25 15:09:09 -0400

    Add test and clean up citation.

--- tedana/model/combine.py
+++ tedana/model/combine.py
@@ -4,50 +4,34 @@ Functions to optimally combine data across echoes.
 import logging
 import numpy as np
 from tedana.utils import unmask
-from tedana.due import due, BibTeX, Doi
+from tedana.due import due, Doi
 
 LGR = logging.getLogger(__name__)
 
 
-@due.dcite(BibTeX('@article{doi:10.1002/(SICI)1522-2594(199907)42:1<87::AID-'
-                  'MRM13>3.0.CO;2-O,'
-                  'author = {Posse Stefan and Wiese Stefan and Gembris Daniel '
-                  'and Mathiak Klaus and Kessler Christoph and Grosse-Ruyken '
-                  'Maria-Liisa and Elghahwagi Barbara and Richards Todd and '
-                  'Dager Stephen R. and Kiselev Valerij G.},'
-                  'title = {Enhancement of BOLD-contrast sensitivity by '
-                  'single-shot multi-echo functional MR imaging},'
-                  'journal = {Magnetic Resonance in Medicine},'
-                  'volume = {42},'
-                  'number = {1},'
-                  'pages = {87-97},'
-                  'doi = {10.1002/(SICI)1522-2594(199907)42:1<87::AID-MRM13>'
-                  '3.0.CO;2-O},'
-                  'url = {https://onlinelibrary.wiley.com/doi/abs/10.1002/'
-                  '%28SICI%291522-2594%28199907%2942%3A1%3C87%3A%3AAID-'
-                  'MRM13%3E3.0.CO%3B2-O}}'),
+@due.dcite(Doi('10.1002/(SICI)1522-2594(199907)42:1<87::AID-MRM13>3.0.CO;2-O'),
            description='T2* method of combining data across echoes using '
                        'monoexponential equation.')
-def _combine_t2s(mdata, tes, ft2s):
+def _combine_t2s(data, tes, ft2s):
     """
     Combine data across echoes using weighted averaging according to voxel-
     (and sometimes volume-) wise estimates of T2*.
 
     Parameters
     ----------
-    mdata : (S x E x T) array_like
+    data : (M x E x T) array_like
         Masked data.
-    tes : (E,) array_like
+    tes : (1 x E) array_like
         Echo times in milliseconds.
-    ft2s : (S [x T]) array_like
+    ft2s : (M [x T] X 1) array_like
         Either voxel-wise or voxel- and volume-wise estimates of T2*.
 
     Returns
     -------
-    combined : (S x T) :obj:`numpy.ndarray`
+    combined : (M x T) :obj:`numpy.ndarray`
         Data combined across echoes according to T2* estimates.
     """
-    n_vols = mdata.shape[-1]
+    n_vols = data.shape[-1]
     alpha = tes * np.exp(-tes / ft2s)
     if alpha.ndim == 2:
         # Voxel-wise T2 estimates
@@ -61,33 +45,33 @@ def _combine_t2s(mdata, tes, ft2s):
         # divide-by-zero errors
         ax0_idx, ax2_idx = np.where(np.all(alpha == 0, axis=1))
         alpha[ax0_idx, :, ax2_idx] = 1.
-    combined = np.average(mdata, axis=1, weights=alpha)
+    combined = np.average(data, axis=1, weights=alpha)
     return combined
 
 
 @due.dcite(Doi('10.1002/mrm.20900'),
            description='STE method of combining data across echoes using just '
                        'SNR/signal and TE.')
-def _combine_ste(mdata, tes):
+def _combine_ste(data, tes):
     """
     Combine data across echoes using SNR/signal and TE.
 
     Parameters
     ----------
-    mdata : (S x E x T) array_like
+    data : (M x E x T) array_like
         Masked data.
-    tes : (E,) array_like
+    tes : (1 x E) array_like
         Echo times in milliseconds.
 
     Returns
     -------
-    combined : (S x T) :obj:`numpy.ndarray`
+    combined : (M x T) :obj:`numpy.ndarray`
         Data combined across echoes according to SNR/signal.
     """
-    n_vols = mdata.shape[-1]
-    alpha = mdata.mean(axis=-1) * tes
+    n_vols = data.shape[-1]
+    alpha = data.mean(axis=-1) * tes
     alpha = np.tile(alpha[:, :, np.newaxis], (1, 1, n_vols))
-    combined = np.average(mdata, axis=1, weights=alpha)
+    combined = np.average(data, axis=1, weights=alpha)
     return combined
 
 
@@ -149,26 +133,27 @@ def make_optcom(data, tes, mask, t2s=None, combmode='t2s', verbose=True):
         raise ValueError("Argument 't2s' must be supplied if 'combmode' is "
                          "set to 't2s'.")
     elif combmode == 'ste' and t2s is not None:
-        LGR.warning("Argument 't2s' is not required if 'combmode' is 'ste'.")
+        LGR.warning("Argument 't2s' is not required if 'combmode' is 'ste'. "
+                    "'t2s' array will not be used.")
 
-    mdata = data[mask, :, :]  # mask out empty voxels/samples
-    tes = np.array(tes)[np.newaxis]  # (1 x E) array_like
+    data = data[mask, :, :]  # mask out empty voxels/samples
+    tes = np.array(tes)[np.newaxis, ...]  # (1 x E) array_like
 
-    if t2s.ndim == 1:
-        msg = 'Optimally combining data with voxel-wise T2 estimates'
-        ft2s = t2s[mask, np.newaxis]
-    else:
-        msg = ('Optimally combining data with voxel- and volume-wise T2 '
-               'estimates')
-        ft2s = t2s[mask, :, np.newaxis]
+    if t2s is not None:
+        if t2s.ndim == 1:
+            msg = 'Optimally combining data with voxel-wise T2 estimates'
+        else:
+            msg = ('Optimally combining data with voxel- and volume-wise T2 '
+                   'estimates')
+        t2s = t2s[mask, ..., np.newaxis]  # mask out empty voxels/samples
 
-    if verbose:
-        LGR.info(msg)
+        if verbose:
+            LGR.info(msg)
 
     if combmode == 'ste':
-        combined = _combine_ste(mdata, tes)
+        combined = _combine_ste(data, tes)
     else:
-        combined = _combine_t2s(mdata, tes, ft2s)
+        combined = _combine_t2s(data, tes, t2s)
 
     combined = unmask(combined, mask)
     return combined
