e9820f111e50b052bf35c4d9c6bbddb1e656e5e6
Taylor Salo <tsalo006@fiu.edu>
2018-08-07 00:19:46 -0400

    Of course there were style issues.

--- tedana/tests/test_model_fit_fitmodels_direct.py
+++ tedana/tests/test_model_fit_fitmodels_direct.py
@@ -25,10 +25,10 @@ def test_break_fitmodels_direct():
 
     catd = np.empty((n_samples+1, n_echos, n_vols))
     with pytest.raises(ValueError) as e_info:
-        _ = fit.fitmodels_direct(catd=catd, mmix=mmix, mask=mask, t2s=t2s,
-                                 t2s_full=t2s_full, tes=tes, combmode=combmode,
-                                 ref_img=ref_img,
-                                 reindex=False, mmixN=None, full_sel=True)
+        fit.fitmodels_direct(catd=catd, mmix=mmix, mask=mask, t2s=t2s,
+                             t2s_full=t2s_full, tes=tes, combmode=combmode,
+                             ref_img=ref_img,
+                             reindex=False, mmixN=None, full_sel=True)
     assert str(e_info.value) == ('First dimensions (number of samples) of '
                                  'catd ({0}), '
                                  't2s ({1}), and mask ({2}) do not '
@@ -38,10 +38,10 @@ def test_break_fitmodels_direct():
     catd = np.empty((n_samples, n_echos, n_vols))
     t2s = np.empty((n_samples+1, n_vols))
     with pytest.raises(ValueError) as e_info:
-        _ = fit.fitmodels_direct(catd=catd, mmix=mmix, mask=mask, t2s=t2s,
-                                 t2s_full=t2s_full, tes=tes, combmode=combmode,
-                                 ref_img=ref_img,
-                                 reindex=False, mmixN=None, full_sel=True)
+        fit.fitmodels_direct(catd=catd, mmix=mmix, mask=mask, t2s=t2s,
+                             t2s_full=t2s_full, tes=tes, combmode=combmode,
+                             ref_img=ref_img,
+                             reindex=False, mmixN=None, full_sel=True)
     assert str(e_info.value) == ('First dimensions (number of samples) of '
                                  'catd ({0}), '
                                  't2s ({1}), and mask ({2}) do not '
@@ -51,10 +51,10 @@ def test_break_fitmodels_direct():
     t2s = np.empty((n_samples, n_vols))
     t2s_full = np.empty((n_samples+1, n_vols))
     with pytest.raises(ValueError) as e_info:
-        _ = fit.fitmodels_direct(catd=catd, mmix=mmix, mask=mask, t2s=t2s,
-                                 t2s_full=t2s_full, tes=tes, combmode=combmode,
-                                 ref_img=ref_img,
-                                 reindex=False, mmixN=None, full_sel=True)
+        fit.fitmodels_direct(catd=catd, mmix=mmix, mask=mask, t2s=t2s,
+                             t2s_full=t2s_full, tes=tes, combmode=combmode,
+                             ref_img=ref_img,
+                             reindex=False, mmixN=None, full_sel=True)
     assert str(e_info.value) == ('First dimensions (number of samples) of '
                                  'catd ({0}), '
                                  't2s ({1}), and mask ({2}) do not '
@@ -64,10 +64,10 @@ def test_break_fitmodels_direct():
     catd = np.empty((n_samples, n_echos+1, n_vols))
     t2s_full = np.empty((n_samples, n_vols))
     with pytest.raises(ValueError) as e_info:
-        _ = fit.fitmodels_direct(catd=catd, mmix=mmix, mask=mask, t2s=t2s,
-                                 t2s_full=t2s_full, tes=tes, combmode=combmode,
-                                 ref_img=ref_img,
-                                 reindex=False, mmixN=None, full_sel=True)
+        fit.fitmodels_direct(catd=catd, mmix=mmix, mask=mask, t2s=t2s,
+                             t2s_full=t2s_full, tes=tes, combmode=combmode,
+                             ref_img=ref_img,
+                             reindex=False, mmixN=None, full_sel=True)
     assert str(e_info.value) == ('Second dimension of catd ({0}) does not '
                                  'match number '
                                  'of echoes provided (tes; '
@@ -75,10 +75,10 @@ def test_break_fitmodels_direct():
 
     catd = np.empty((n_samples, n_echos, n_vols+1))
     with pytest.raises(ValueError) as e_info:
-        _ = fit.fitmodels_direct(catd=catd, mmix=mmix, mask=mask, t2s=t2s,
-                                 t2s_full=t2s_full, tes=tes, combmode=combmode,
-                                 ref_img=ref_img,
-                                 reindex=False, mmixN=None, full_sel=True)
+        fit.fitmodels_direct(catd=catd, mmix=mmix, mask=mask, t2s=t2s,
+                             t2s_full=t2s_full, tes=tes, combmode=combmode,
+                             ref_img=ref_img,
+                             reindex=False, mmixN=None, full_sel=True)
     assert str(e_info.value) == ('Third dimension (number of volumes) of catd ({0}) '
                                  'does not match first dimension of '
                                  'mmix ({1})'.format(catd.shape[2], mmix.shape[0]))
@@ -86,19 +86,19 @@ def test_break_fitmodels_direct():
     catd = np.empty((n_samples, n_echos, n_vols))
     t2s = np.empty((n_samples, n_vols+1))
     with pytest.raises(ValueError) as e_info:
-        _ = fit.fitmodels_direct(catd=catd, mmix=mmix, mask=mask, t2s=t2s,
-                                 t2s_full=t2s_full, tes=tes, combmode=combmode,
-                                 ref_img=ref_img,
-                                 reindex=False, mmixN=None, full_sel=True)
+        fit.fitmodels_direct(catd=catd, mmix=mmix, mask=mask, t2s=t2s,
+                             t2s_full=t2s_full, tes=tes, combmode=combmode,
+                             ref_img=ref_img,
+                             reindex=False, mmixN=None, full_sel=True)
     assert str(e_info.value) == ('Shape of t2s array {0} does not match shape of '
                                  't2s_full array {1}'.format(t2s.shape, t2s_full.shape))
 
     t2s_full = np.empty((n_samples, n_vols+1))
     with pytest.raises(ValueError) as e_info:
-        _ = fit.fitmodels_direct(catd=catd, mmix=mmix, mask=mask, t2s=t2s,
-                                 t2s_full=t2s_full, tes=tes, combmode=combmode,
-                                 ref_img=ref_img,
-                                 reindex=False, mmixN=None, full_sel=True)
+        fit.fitmodels_direct(catd=catd, mmix=mmix, mask=mask, t2s=t2s,
+                             t2s_full=t2s_full, tes=tes, combmode=combmode,
+                             ref_img=ref_img,
+                             reindex=False, mmixN=None, full_sel=True)
     assert str(e_info.value) == ('Third dimension (number of volumes) of catd ({0}) '
                                  'does not match second dimension of '
                                  't2s ({1})'.format(catd.shape[2], t2s.shape[1]))
--- tedana/tests/test_model_fit_get_coeffs.py
+++ tedana/tests/test_model_fit_get_coeffs.py
@@ -48,19 +48,19 @@ def test_break_get_coeffs():
 
     data = np.empty((n_samples))
     with pytest.raises(ValueError) as e_info:
-        _ = fit.get_coeffs(data, X, mask, add_const=False)
+        fit.get_coeffs(data, X, mask, add_const=False)
     assert str(e_info.value) == ('Parameter data should be 2d or 3d, not {0}d'.format(data.ndim))
 
     data = np.empty((n_samples, n_vols))
     X = np.empty((n_vols))
     with pytest.raises(ValueError) as e_info:
-        _ = fit.get_coeffs(data, X, mask, add_const=False)
+        fit.get_coeffs(data, X, mask, add_const=False)
     assert str(e_info.value) == ('Parameter X should be 2d, not {0}d'.format(X.ndim))
 
     data = np.empty((n_samples, n_echos, n_vols+1))
     X = np.empty((n_vols, n_comps))
     with pytest.raises(ValueError) as e_info:
-        _ = fit.get_coeffs(data, X, mask, add_const=False)
+        fit.get_coeffs(data, X, mask, add_const=False)
     assert str(e_info.value) == ('Last dimension (dimension {0}) of data ({1}) does not '
                                  'match first dimension of '
                                  'X ({2})'.format(data.ndim, data.shape[-1], X.shape[0]))
@@ -68,11 +68,11 @@ def test_break_get_coeffs():
     data = np.empty((n_samples, n_echos, n_vols))
     mask = np.empty((n_samples, n_echos, n_vols))
     with pytest.raises(ValueError) as e_info:
-        _ = fit.get_coeffs(data, X, mask, add_const=False)
+        fit.get_coeffs(data, X, mask, add_const=False)
     assert str(e_info.value) == ('Parameter data should be 1d or 2d, not {0}d'.format(mask.ndim))
 
     mask = np.empty((n_samples+1, n_echos))
     with pytest.raises(ValueError) as e_info:
-        _ = fit.get_coeffs(data, X, mask, add_const=False)
+        fit.get_coeffs(data, X, mask, add_const=False)
     assert str(e_info.value) == ('First dimensions of data ({0}) and mask ({1}) do not '
                                  'match'.format(data.shape[0], mask.shape[0]))
