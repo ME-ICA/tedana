f7bb326b51738a5fcea02ab3635576e0a04ff12a
Taylor Salo <tsalo006@fiu.edu>
2018-10-13 15:26:20 -0400

    Try changing the tests a bit.

--- tedana/tests/test_tedana.py
+++ tedana/tests/test_tedana.py
@@ -27,8 +27,13 @@ def compare_nifti(fn, test_dir, res_dir):
     """
     res_fp = (res_dir/fn).as_posix()
     test_fp = (test_dir/fn).as_posix()
-    passed = np.allclose(nib.load(res_fp).get_data(),
-                         nib.load(test_fp).get_data())
+    assert op.isfile(test_fp), 'File {0} DNE'.format(test_fp)
+
+    if op.isfile(test_fp):
+        passed = np.allclose(nib.load(res_fp).get_data(),
+                             nib.load(test_fp).get_data())
+    else:
+        passed = False
     return passed
 
 
