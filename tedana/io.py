a242753b05f85c7b57b9fb2057824494f079ac37
Logan Dowdle <logan.dowdle@gmail.com>
2019-01-30 13:20:13 -0500

    fixing plot title

--- tedana/io.py
+++ tedana/io.py
@@ -573,9 +573,8 @@ def writefigures(ts, mask, comptable, mmix, n_vols,
         ax_ts.plot(mmix[:, compnum], color=line_color)
 
         # Title will include variance from comptable
-        plt_title = ('Component ', str(compnum), ' timeseries, ',
-                     "{0:.2f}".format(comptable.iloc[compnum][3]),
-                     "% variance")
+        comp_var = "{0:.2f}".format(comptable.iloc[compnum][3])
+        plt_title = 'Comp. ' + str(compnum), + ': ' + comp_var + '% variance'
         ax_ts.set_title(plt_title)
         ax_ts.set_xlabel('TRs')
         ax_ts.set_xbound(0, n_vols)
