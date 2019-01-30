ea5df152bb1a904a880a257858b151cf824d38b3
Logan Dowdle <logan.dowdle@gmail.com>
2019-01-30 13:04:26 -0500

    include figures in tedana_workflow  args

--- tedana/workflows/tedana.py
+++ tedana/workflows/tedana.py
@@ -157,7 +157,7 @@ def tedana_workflow(data, tes, mask=None, mixm=None, ctab=None, manacc=None,
                     tedort=False, gscontrol=None, tedpca='mle',
                     ste=-1, combmode='t2s', verbose=False, viwer=False,
                     stabilize=False, wvpca=False, out_dir='.', fixed_seed=42,
-                    debug=False, quiet=False):
+                    debug=False, quiet=False, figures=False):
     """
     Run the "canonical" TE-Dependent ANAlysis workflow.
 
