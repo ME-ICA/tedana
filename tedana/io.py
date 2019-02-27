0734a147adedd03fb452878820b6da5457b8f949
Taylor Salo <tsalo006@fiu.edu>
2019-02-27 07:54:14 -0500

    Undo function removal.

--- tedana/io.py
+++ tedana/io.py
@@ -395,6 +395,33 @@ def writeresults_echoes(catd, mmix, mask, acc, rej, midk, ref_img):
                        suffix='e%i' % (i_echo + 1))
 
 
+def ctabsel(ctabfile):
+    """
+    Loads a pre-existing component table file
+
+    Parameters
+    ----------
+    ctabfile : :obj:`str`
+        Filepath to existing component table
+
+    Returns
+    -------
+    ctab : (4,) :obj:`tuple` of :obj:`numpy.ndarray`
+        Tuple containing arrays of (1) accepted, (2) rejected, (3) mid, and (4)
+        ignored components
+    """
+
+    with open(ctabfile, 'r') as src:
+        ctlines = src.readlines()
+    class_tags = ['#ACC', '#REJ', '#MID', '#IGN']
+    class_dict = {}
+    for ii, ll in enumerate(ctlines):
+        for kk in class_tags:
+            if ll[:4] is kk and ll[4:].strip() is not '':
+                class_dict[kk] = ll[4:].split('#')[0].split(',')
+    return tuple([np.array(class_dict[kk], dtype=int) for kk in class_tags])
+
+
 def new_nii_like(ref_img, data, affine=None, copy_header=True):
     """
     Coerces `data` into NiftiImage format like `ref_img`
