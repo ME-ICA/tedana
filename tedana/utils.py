d1a1080a4281f240ef2ae0813fe6613896cb739b
Taylor Salo <tsalo006@fiu.edu>
2019-03-08 07:43:51 -0500

    Clean up masking method.

--- tedana/utils.py
+++ tedana/utils.py
@@ -84,30 +84,33 @@ def make_adaptive_mask(data, mask=None, getsum=False):
         Valued array indicating the number of echos with sufficient signal in a
         given voxel. Only returned if `getsum = True`
     """
-    masksum = np.zeros(data.shape[0])
-    old_mean_echo_data = np.mean(data[:, 0, :], axis=-1)
-    for i_echo in range(1, data.shape[1]):
-        echo_data = data[:, i_echo, :]
-        mean_echo_data = np.mean(echo_data, axis=-1)
-        # If a voxel's signal goes up from one echo to next, that's bad.
-        bad_voxels = mean_echo_data > old_mean_echo_data
-        old_mean_echo_data = mean_echo_data
-        open_voxels = masksum == 0
-        bad_voxels = np.logical_and(bad_voxels, open_voxels)
-        masksum[bad_voxels] = i_echo
+    masksum = np.ones(data.shape[0]) * data.shape[1]
 
-    # take temporal mean of echos and extract non-zero values in first echo
     echo_means = data.mean(axis=-1)  # temporal mean of echos
+
+    # New, coarse method for identifying bad echoes
+    prev_echo_means = echo_means[:, 0]
+    for i_echo in range(1, data.shape[1]):
+        cur_echo_means = echo_means[:, i_echo]
+        unindexed_voxels = masksum == data.shape[1]
+        # If a voxel's signal goes up from one echo to next, that's bad
+        bad_voxels = cur_echo_means > prev_echo_means
+        bad_voxels = np.logical_and(bad_voxels, unindexed_voxels)
+        masksum[bad_voxels] = i_echo
+        prev_echo_means = cur_echo_means
+
+    # Old, fine method for identifying bad echoes
+    # take temporal mean of echos and extract non-zero values in first echo
     first_echo = echo_means[echo_means[:, 0] != 0, 0]
 
-    # get 33rd %ile of `first_echo` and find corresponding index
+    # get 33rd %ile of first_echo and find corresponding index
     # NOTE: percentile is arbitrary
     perc = np.percentile(first_echo, 33, interpolation='higher')
-    perc_val = (echo_means[:, 0] == perc)
+    perc_val_idx = echo_means[:, 0] == perc
 
     # extract values from all echos at relevant index
     # NOTE: threshold of 1/3 voxel value is arbitrary
-    lthrs = np.squeeze(echo_means[perc_val].T) / 3
+    lthrs = np.squeeze(echo_means[perc_val_idx, ...].T) / 3
 
     # if multiple samples were extracted per echo, keep the one w/the highest signal
     if lthrs.ndim > 1:
@@ -116,8 +119,11 @@ def make_adaptive_mask(data, mask=None, getsum=False):
     # determine samples where absolute value is greater than echo-specific thresholds
     # and count # of echos that pass criterion
     temp_masksum = (np.abs(echo_means) > lthrs).sum(axis=-1)
-    masksum[masksum == 0] = temp_masksum[masksum == 0]
 
+    # combine results from coarse and fine methods
+    masksum = np.minimum(masksum, temp_masksum)
+
+    # Finalize masks
     if mask is None:
         # make it a boolean mask to (where we have at least 1 echo with good signal)
         mask = masksum.astype(bool)
