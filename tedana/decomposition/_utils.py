59dc5253b05a7d4ea3ed8008d9545ec35affdb76
Taylor Salo <tsalo006@fiu.edu>
2019-03-11 13:23:08 -0400

    Remove WVPCA support.

--- tedana/decomposition/_utils.py
+++ tedana/decomposition/_utils.py
@@ -3,7 +3,6 @@ Utility functions for tedana decomposition
 """
 import logging
 
-import pywt
 import numpy as np
 from scipy import stats
 
@@ -44,65 +43,3 @@ def eimask(dd, ees=None):
         imask[np.logical_and(m > lthr, m < hthr), ee] = True
 
     return imask
-
-
-def dwtmat(mmix):
-    """
-    Wavelet transform data using order 2 Daubechies wavelet.
-
-    Parameters
-    ----------
-    mmix : {S, T} :obj:`numpy.ndarray`
-        Data to wavelet transform.
-
-    Returns
-    -------
-    mmix_wt : {S, 2C} :obj:`numpy.ndarray`
-        Wavelet-transformed data. Approximation and detail coefficients are
-        horizontally concatenated for each row in mmix.
-    n_coefs_approx : :obj:`int`
-        The number of approximation coefficients from the wavelet
-        transformation. Used to split the wavelet-transformed data into
-        approximation and detail coefficients.
-    """
-    n_samp = mmix.shape[0]
-    coefs_tup = pywt.dwt(mmix[0, :], 'db2')
-    n_coefs_approx = len(coefs_tup[0])
-    n_coefs_total = len(np.hstack(coefs_tup))
-    mmix_wt = np.zeros((n_samp, n_coefs_total))
-    for i_samp in range(n_samp):
-        coefs_tup = pywt.dwt(mmix[i_samp], 'db2')
-        mmix_wt[i_samp] = np.hstack(coefs_tup)
-
-    return mmix_wt, n_coefs_approx
-
-
-def idwtmat(mmix_wt, n_coefs_approx):
-    """
-    Invert wavelet transform data with order 2 Daubechies wavelet.
-
-    Parameters
-    ----------
-    mmix_wt : {S, 2C} :obj:`numpy.ndarray`
-        Wavelet-transformed data. Approximation and detail coefficients are
-        horizontally concatenated for each row in mmix.
-    n_coefs_approx : :obj:`int`
-        The number of approximation coefficients from the wavelet
-        transformation. Used to split the wavelet-transformed data into
-        approximation and detail coefficients.
-
-    Returns
-    -------
-    mmix_iwt : {S, T} :obj:`numpy.ndarray`
-        Inverse wavelet-transformed data.
-    """
-    n_samp = mmix_wt.shape[0]
-    coefs_approx = mmix_wt[:, :n_coefs_approx]
-    coefs_detail = mmix_wt[:, n_coefs_approx:]
-    n_trs = len(pywt.idwt(coefs_approx[0, :], coefs_detail[0, :], 'db2'))
-    mmix_iwt = np.zeros((n_samp, n_trs))
-    for i_samp in range(n_samp):
-        mmix_iwt[i_samp] = pywt.idwt(coefs_approx[i_samp, :],
-                                     coefs_detail[i_samp, :], 'db2')
-
-    return mmix_iwt
