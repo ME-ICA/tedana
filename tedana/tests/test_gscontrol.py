b1cd29ddd0dc4c6c31846c61be6a13be5a52e714
Taylor Salo <tsalo006@fiu.edu>
2019-04-17 22:36:54 -0400

    Fix test.

--- tedana/tests/test_gscontrol.py
+++ tedana/tests/test_gscontrol.py
@@ -14,29 +14,29 @@ def test_break_gscontrol_raw():
     shapes.
     """
     n_samples, n_echos, n_vols = 10000, 4, 100
-    catd = np.empty((n_samples, n_echos, n_vols))
-    optcom = np.empty((n_samples, n_vols))
+    data_cat = np.empty((n_samples, n_echos, n_vols))
+    data_oc = np.empty((n_samples, n_vols))
     ref_img = ''
 
-    catd = np.empty((n_samples+1, n_echos, n_vols))
+    data_cat = np.empty((n_samples+1, n_echos, n_vols))
     with pytest.raises(ValueError) as e_info:
-        gsc.gscontrol_raw(catd=catd, optcom=optcom, n_echos=n_echos,
+        gsc.gscontrol_raw(data_cat=data_cat, data_oc=data_oc, n_echos=n_echos,
                           ref_img=ref_img, dtrank=4)
-    assert str(e_info.value) == ('First dimensions of catd ({0}) and optcom ({1}) do not '
-                                 'match'.format(catd.shape[0], optcom.shape[0]))
+    assert str(e_info.value) == ('First dimensions of data_cat ({0}) and data_oc ({1}) do not '
+                                 'match'.format(data_cat.shape[0], data_oc.shape[0]))
 
-    catd = np.empty((n_samples, n_echos+1, n_vols))
+    data_cat = np.empty((n_samples, n_echos+1, n_vols))
     with pytest.raises(ValueError) as e_info:
-        gsc.gscontrol_raw(catd=catd, optcom=optcom, n_echos=n_echos,
+        gsc.gscontrol_raw(data_cat=data_cat, data_oc=data_oc, n_echos=n_echos,
                           ref_img=ref_img, dtrank=4)
-    assert str(e_info.value) == ('Second dimension of catd ({0}) does not match '
-                                 'n_echos ({1})'.format(catd.shape[1], n_echos))
+    assert str(e_info.value) == ('Second dimension of data_cat ({0}) does not match '
+                                 'n_echos ({1})'.format(data_cat.shape[1], n_echos))
 
-    catd = np.empty((n_samples, n_echos, n_vols))
-    optcom = np.empty((n_samples, n_vols+1))
+    data_cat = np.empty((n_samples, n_echos, n_vols))
+    data_oc = np.empty((n_samples, n_vols+1))
     with pytest.raises(ValueError) as e_info:
-        gsc.gscontrol_raw(catd=catd, optcom=optcom, n_echos=n_echos,
+        gsc.gscontrol_raw(data_cat=data_cat, data_oc=data_oc, n_echos=n_echos,
                           ref_img=ref_img, dtrank=4)
-    assert str(e_info.value) == ('Third dimension of catd ({0}) does not match '
-                                 'second dimension of optcom '
-                                 '({1})'.format(catd.shape[2], optcom.shape[1]))
+    assert str(e_info.value) == ('Third dimension of data_cat ({0}) does not match '
+                                 'second dimension of data_oc '
+                                 '({1})'.format(data_cat.shape[2], data_oc.shape[1]))
