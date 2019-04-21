bb6b8288c6cab366e6fc147c309fb7ada9512552
Taylor Salo <tsalo006@fiu.edu>
2019-04-21 15:41:07 -0400

    Fix workflow.

--- tedana/workflows/tedana.py
+++ tedana/workflows/tedana.py
@@ -350,11 +350,11 @@ def tedana_workflow(data, tes, mask=None, mixm=None, ctab=None, manacc=None,
 
     if mixm is None:
         # Identify and remove thermal noise from data
-        n_components, dd = decomposition.tedpca(catd, data_oc, combmode, mask,
-                                                t2s, t2sG, ref_img,
+        n_components, dd = decomposition.tedpca(catd, data_oc, mask,
+                                                t2s, ref_img,
                                                 tes=tes, method=tedpca, ste=ste,
                                                 kdaw=10., rdaw=1.,
-                                                verbose=verbose)
+                                                out_dir=out_dir, verbose=verbose)
         mmix_orig = decomposition.tedica(n_components, dd, fixed_seed,
                                          maxit, maxrestart)
 
