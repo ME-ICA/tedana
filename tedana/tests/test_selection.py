f55a1e9261058089e9d18987d5ebc4df2b1bc2d5
Taylor Salo <tsalo006@fiu.edu>
2019-04-22 07:36:13 -0400

    Add new tests.

---
+++ tedana/tests/test_selection.py
@@ -0,0 +1,31 @@
+"""
+Tests for tedana.model.monoexponential
+"""
+
+import os.path as op
+
+import pytest
+import numpy as np
+import pandas as pd
+
+from tedana import selection
+
+
+def test_manual_selection():
+    comptable = pd.DataFrame(index=np.arange(100))
+    comptable = selection.manual_selection(comptable, acc=[1, 3, 5])
+    assert comptable.loc[comptable.classification == 'accepted'].shape[0] == 3
+    assert (comptable.loc[comptable.classification == 'rejected'].shape[0] ==
+            (comptable.shape[0] - 3))
+
+    comptable = selection.manual_selection(comptable, rej=[1, 3, 5])
+    assert comptable.loc[comptable.classification == 'rejected'].shape[0] == 3
+    assert (comptable.loc[comptable.classification == 'accepted'].shape[0] ==
+            (comptable.shape[0] - 3))
+
+    comptable = selection.manual_selection(comptable, acc=[0, 2, 4],
+                                           rej=[1, 3, 5])
+    assert comptable.loc[comptable.classification == 'accepted'].shape[0] == 3
+    assert comptable.loc[comptable.classification == 'rejected'].shape[0] == 3
+    assert (comptable.loc[comptable.classification == 'ignored'].shape[0] ==
+            comptable.shape[0] - 6)
