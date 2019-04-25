f40249f15b7ffe13a742704072e113153f05f420
Taylor Salo <tsalo006@fiu.edu>
2019-04-25 09:57:25 -0400

    Fix init file.

--- tedana/selection/__init__.py
+++ tedana/selection/__init__.py
@@ -2,6 +2,6 @@
 # ex: set sts=4 ts=4 sw=4 et:
 
 from .tedpca import kundu_tedpca
-]from .tedica import kundu_selection_v2, manual_selection
+from .tedica import kundu_selection_v2, manual_selection
 
 __all__ = ['kundu_tedpca', 'kundu_selection_v2', 'manual_selection']
