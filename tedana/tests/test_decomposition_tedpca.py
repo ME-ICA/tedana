9eeb6610987d9b39ab607dd1c590d063b3834939
Sourav Singh <ssouravsingh12@gmail.com>
2019-04-27 01:37:42 +0530

    Add test for breaking tedpca

---
+++ tedana/tests/test_decomposition_tedpca.py
@@ -0,0 +1,29 @@
+"""
+Tests for tedana.decomposition.tedpca
+"""
+
+import numpy as np
+import pytest
+
+from tedana.decomposition import tedpca
+
+
+def test_break_tedpca():
+    """
+    Ensure that computefeats2 fails when input data do not have the right
+    shapes.
+    """
+    n_samples, n_echos, n_time = 10000, 4, 50
+    data_cat = np.empty((n_samples, n_echos, n_time))
+    data_oc = np.empty((n_samples, n_time))
+    combmode = 't2s'
+    mask = np.empty((n_samples))
+    t2s = np.empty((n_samples))
+    t2sG = np.empty((n_samples))
+    ref_img = ''
+    tes = ''
+    method = 'mle'
+    
+    with pytest.raises(ValueError) as e_info:
+        tedpca(data_cat, data_oc, combmode, mask, t2s, t2sG, ref_img, tes, method)
+    assert str(e_info.value) == ('Parameter data should be 2d, not {0}d'.format(data.ndim))
