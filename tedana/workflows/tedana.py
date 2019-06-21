9dd1c7b6ce1a71f08933735cfc28871590769f88
Joshua Teves <jbtevespro@gmail.com>
2019-06-21 16:09:15 -0400

    [FIX] Add TR checking (#333)
    Co-authored-by: Logan Dowdle <logan.dowdle@gmail.com>

--- tedana/workflows/tedana.py
+++ tedana/workflows/tedana.py
@@ -311,6 +311,17 @@ def tedana_workflow(data, tes, mask=None, mixm=None, ctab=None, manacc=None,
     n_samp, n_echos, n_vols = catd.shape
     LGR.debug('Resulting data shape: {}'.format(catd.shape))
 
+    # check if TR is 0
+    img_t_r = ref_img.header.get_zooms()[-1]
+    if img_t_r == 0 and png:
+        raise IOError('Dataset has a TR of 0. This indicates incorrect'
+                      ' header information. To correct this, we recommend'
+                      ' using this snippet:'
+                      '\n'
+                      'https://gist.github.com/jbteves/032c87aeb080dd8de8861cb151bff5d6'
+                      '\n'
+                      'to correct your TR to the value it should be.')
+
     if mixm is not None and op.isfile(mixm):
         mixm = op.abspath(mixm)
         # Allow users to re-run on same folder
