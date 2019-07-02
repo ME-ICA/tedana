5cf493be22912b257bf12b4815e43e72ac37a425
smoia <s.moia@bcbl.eu>
2019-07-02 19:51:07 +0200

    Added numpy docstring infos

--- tedana/workflows/post_taskcorr.py
+++ tedana/workflows/post_taskcorr.py
@@ -1,17 +1,26 @@
+"""
+Quick hack for tedana, from HBM Hackathon 2019.
+TODO:
+- let user decide wether they want NCC or simple correlation.
+- Better threshold.
+
+"""
+
 import logging
 
 import sys
-import os.path as op
 import argparse
-import scipy.stats as sct
+
 import numpy as np
 import pandas as pd
+
+import os.path as op
+import scipy.stats as sct
+
 from tedana.workflows.parser_utils import is_valid_file
 
-# import sys
-
 LGR = logging.getLogger(__name__)
-
+logging.basicConfig(level=logging.INFO)
 
 def _get_parser():
     """
@@ -20,6 +29,7 @@ def _get_parser():
     Returns
     -------
     parser.parse_args() : argparse dict
+
     """
     parser = argparse.ArgumentParser()
     # Argument parser follow template provided by RalphyZ, also used by tedana.
@@ -33,11 +43,6 @@ def _get_parser():
                                 'the mixing matrix and the component'
                                 'table are located.'),
                           required=True)
-    required.add_argument('-dmf', '--dmat_dir',
-                          dest='dmat_dir',
-                          type=str,
-                          help=('Directory containing the design matrices.'),
-                          required=True)
     optional.add_argument('-gts', '--good_ts',
                           dest='good_ts',
                           metavar='FILE',
@@ -45,7 +50,7 @@ def _get_parser():
                           help=('File containing timeseries that should'
                                 'be accepted. It can be a 1D array or a'
                                 'matrix, where columns are the timeseries.'
-                                'Specify with path.'),
+                                'Specify with full path.'),
                           default=None)
     optional.add_argument('-bts', '--bad_ts',
                           dest='bad_ts',
@@ -54,7 +59,7 @@ def _get_parser():
                           help=('File containing timeseries that should'
                                 'be rejected. It can be a 1D array or a'
                                 'matrix, where columns are the timeseries'
-                                'Specify with path.'),
+                                'Specify with full path.'),
                           default=None)
     optional.add_argument('-t', '--thr',
                           dest='thr',
@@ -68,6 +73,18 @@ def _get_parser():
 def import_file(file_name):
     """
     Importing files and normalising them for Normalised Cross-Correlation
+
+    Parameters
+    ----------
+    file_name: string
+        Name of the file to be imported, with full path.
+
+    Returns
+    -------
+    norm_ts: T x N numpy.array
+        Normalised timeseries matrix.
+        T is number of timepoints, N is number of timeseries.
+
     """
     timeseries = np.genfromtxt(file_name)
     norm_ts = sct.zscore(timeseries, axis=0)
@@ -76,17 +93,35 @@ def import_file(file_name):
 
 def check_dimensionality(ts, mixmat):
     """
-    Just checking ts is rightly oriented
+    Just checking that ts is rightly oriented
+
+    Parameters
+    ----------
+    ts: T x N or N x T numpy.array
+        Array of unkown dimensionality to be compared with mixmat.
+        Its first dimension length should match mixmat.
+    mixmat: T x N numpy.array
+        Array of known dimensionality, to which ts is compared to.
+
+    Returns
+    -------
+    ts: T x N numpy.array
+        Array of known dimensionality. Its first dimension is the same as mixmat.
+
+    Raise
+    -----
+    sys.exit if ts and mixmat don't have any dimension of same length.
+
     """
     ntr, _ = mixmat.shape
     ts_shape = ts.shape
     # Add a general check on matching at least one dimension
     if ts_shape[0] != ntr:
         if np.size(ts.shape) == 1 or ts_shape[1] != ntr:
-            LGR.info('Input matrix has wrong dimensionality.')
+            LGR.error('Input matrix has wrong dimensionality.')
             sys.exit()
         else:
-            LGR.info('Wrong orientation, transposing the design matrix.')
+            LGR.warning('Wrong orientation, transposing the design matrix.')
             ts = ts.T
 
     return ts
@@ -96,7 +131,23 @@ def correlate_ts(ts, mixmat, thr=0.6):
     """
     Run (normalised) cross correlation of the signals
     And then selects maximum NCC to decide which one to flag.
-    Also threshold components and then save their position
+    Also threshold components and then save their index.
+
+    Parameters
+    ----------
+    ts: T x N numpy.array
+        Timeseries to compare components to.
+    mixmat: T x N numpy.array
+        Timeseries of MEICA components.
+    thr: float, Optional
+        Threshold for NCC value considered significant. Default = 0.6.
+
+    Returns
+    -------
+    selcomp: list
+        List of indexes of components that are significantly similar to any of the N
+        timeseries of ts.
+
     """
     _, ncomp = mixmat.shape
 
@@ -121,8 +172,22 @@ def correlate_ts(ts, mixmat, thr=0.6):
 
 def modify_comp_table(ctab_fullpath, selcomp, flag='accepted'):
     """
-    This function requires as input the component table,
-    the index of the selected components and a flag that will overwrite the one in the table.
+    Function to change flag of components in the components table.
+
+    Parameters
+    ----------
+    ctab_fullpath: string
+        Full path to components table as of tedana output.
+        !!! It will be overwritten !!!
+    selcomp: list
+        List of indexes of components to be flagged with flag.
+    flag: string, Optional
+        Flag to change the label of the components with index
+        selcomp, in the components table.
+
+    Note
+    ----
+    The file output is the same ctab_fullpath
 
     """
 
@@ -131,7 +196,7 @@ def modify_comp_table(ctab_fullpath, selcomp, flag='accepted'):
     comptable['original_classification'] = comptable['classification']
     comptable['original_rationale'] = comptable['rationale']
     LGR.info('Overriding original classifications with correlation with'
-             'input timeseries (' + flag + ' components)')
+             'input timeseries (%s components)', flag)
     comptable.loc[selcomp, 'classification'] = flag
     if flag == 'accepted':
         comptable.loc[selcomp, 'rationale'] = 'I099'
@@ -144,18 +209,21 @@ def modify_comp_table(ctab_fullpath, selcomp, flag='accepted'):
 
 def check_task_corr(mixmat, ctab_fullpath, ts, thr, flag='accepted'):
     """
-    This function is the workflow of the component selection
+    This function is the workflow of the component selection.
     """
     norm_ts = import_file(ts)
     checked_ts = check_dimensionality(norm_ts, mixmat)
     selcomp = correlate_ts(checked_ts, mixmat, thr)
-    modify_comp_table(ctab_fullpath, selcomp, flag)
+    if selcomp:
+        modify_comp_table(ctab_fullpath, selcomp, flag)
+    else:
+        LGR.info('No component to flag as %s', flag)
 
 
 def _main(argv=None):
     options = _get_parser().parse_args(argv)
     ctab_fullpath = op.join(options.tedana_dir, 'comp_table_ica.txt')
-    mixm_fullpath = op.join(options.tedana_dir, '__meica_mix.1D')
+    mixm_fullpath = op.join(options.tedana_dir, 'meica_mix.1D')
 
     mixmat = import_file(mixm_fullpath)
 
