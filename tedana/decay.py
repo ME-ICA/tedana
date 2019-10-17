75064cbf023a3b2b8563fa167b24347a7e406207
Logan <logan.dowdle@gmail.com>
2019-10-17 11:46:28 -0500

    worse than missing 1 ')', is missing 2

--- tedana/decay.py
+++ tedana/decay.py
@@ -139,7 +139,7 @@ def fit_decay(data, tes, mask, masksum, fittype):
                     fail_count += 1
                     pass
             if fail_count:
-                fail_percent = (fail_count / t2s.size * 100))
+                fail_percent = (fail_count / t2s.size * 100)
                 LGR.debug('With {0} echoes, monoexponential fit failed on {1} ({2:.2f}%) voxel(s),'
                           ' used log linear estimate instead'.format(echo_num + 1, fail_count,
                                                                      fail_percent))
