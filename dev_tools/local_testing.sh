888de0fd29324625e41646f0bfeb6f3d7b9236ed
Ross Markello <rossmarkello@gmail.com>
2019-11-06 15:13:15 -0500

    [TEST] Update Dockerfile to match new integr tests

--- dev_tools/local_testing.sh
+++ dev_tools/local_testing.sh
@@ -1,28 +1,24 @@
 #!/usr/bin/env bash
 
-generate_tedana_dockerfile() {
+update_tedana_dockerfile() {
     #
     # Generates Dockerfile to build Docker image for local tedana testing
     #
 
-    get_three_echo_data='
-        mkdir -p /data/three-echo
-        && curl -L -o /data/three-echo/three_echo_Cornell_zcat.nii.gz https://osf.io/8fzse/download'
-    get_five_echo_data='
-        mkdir /data/five-echo
-        && curl -L -o five_echo_NIH.tar.xz https://osf.io/ea5v3/download
-        && tar xf five_echo_NIH.tar.xz -C /data/five-echo
-        && rm -f five_echo_NIH.tar.xz'
-    get_three_echo_reg='
-        mkdir -p /data/test/three-echo
+    get_three_echo_test_data='
+        mkdir -p /data/three-echo /data/test/three-echo
+        && curl -L -o /data/three-echo/three_echo_Cornell_zcat.nii.gz https://osf.io/8fzse/download
         && curl -L -o TED.Cornell_processed_three_echo_dataset.tar.xz https://osf.io/u65sq/download
         && tar xf TED.Cornell_processed_three_echo_dataset.tar.xz --no-same-owner -C /data/test/three-echo/
         && rm -f TED.Cornell_processed_three_echo_dataset.tar.xz'
-    get_five_echo_reg='
-        mkdir -p /data/test/five-echo
+    get_five_echo_test_data='
+        mkdir /data/five-echo /data/test/five-echo
+        && curl -L -o five_echo_NIH.tar.xz https://osf.io/ea5v3/download
+        && tar xf five_echo_NIH.tar.xz -C /data/five-echo
+        && rm -f five_echo_NIH.tar.xz
         && curl -L -o TED.p06.tar.xz https://osf.io/fr6mx/download
         && tar xf TED.p06.tar.xz --no-same-owner -C /data/test/five-echo/
-        && rm -f TED.p06.tar.xz'
+        && rm -f TED.p06.tar.xz'2
     generate_ipython_config="
         /opt/conda/envs/tedana_py36/bin/ipython profile create
         && sed -i 's/#c.InteractiveShellApp.extensions = \[\]/c.InteractiveShellApp.extensions = \['\''autoreload'\''\]/g' /root/.ipython/profile_default/ipython_config.py"
@@ -48,10 +44,8 @@ generate_tedana_dockerfile() {
                   install_path=/opt/conda                                      \
                   yaml_file=/dev_tools/envs/py37_env.yml                       \
                   activate_env=false                                           \
-      --run "${get_three_echo_data}"                                           \
-      --run "${get_five_echo_data}"                                            \
-      --run "${get_three_echo_reg}"                                            \
-      --run "${get_five_echo_reg}"                                             \
+      --run "${get_three_echo_test_data}"                                      \
+      --run "${get_five_echo_test_data}"                                       \
       --run "${generate_ipython_config}"                                       \
       --copy "./dev_tools/local_testing.sh" /dev_tools/local_testing.sh        \
       --add-to-entrypoint "source activate tedana_py36"                        \
@@ -72,7 +66,7 @@ build_tedana_image() {
         tag=local
     fi
 
-    generate_tedana_dockerfile
+    update_tedana_dockerfile
     docker build --tag tedana/tedana-dev:${tag} -f Dockerfile_dev .
 }
 
@@ -94,76 +88,15 @@ cprint() {
 }
 
 
-_check_tedana_outputs() {
-    #
-    # Runs tedana unit tests for specified Python version / virtual environment
-    #
-    # Required argments:
-    #   dataset         name of dataset to use for testing. should be one of
-    #                   [three-echo, five-echo]
-
-    # confirm specification of three-echo or five-echo input
-    if [ -z "${1}" ] || { [ "${1}" != "three-echo" ] && [ "${1}" != "five-echo" ]; }; then
-        printf 'Must supply dataset name for checking integration test ' >&2
-        printf 'outputs; must be one of [three-echo, five-echo]\n' >&2
-        return
-    fi
-
-    # find file
-    find /data/"${1}"/TED."${1}"/* \
-        -exec basename {} \; > /data/"${1}"/TED."${1}"/outputs.out
-
-    # set filenames
-    f1=/tedana/.circleci/tedana_outputs.txt
-    f2=/data/"${1}"/TED."${1}"/outputs.out
-
-    # sort both files, pipe into grep to check for tedana
-    # logfile format; should only see one
-    comm -13 <(sort -u $f1) <(sort -u $f2) | grep -E -e '^tedana_[12][0-9]{3}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.txt$'
-    numlogs=$(comm -13 <(sort -u $f1) <(sort -u $f2) | grep -E -e '^tedana_[12][0-9]{3}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.txt$' | wc -l)
-    if [[ ! $numlogs -eq 1 ]]; then
-    printf "Incorrect number of logfiles: %s" $numlogs
-    fi
-
-    # verify non-log outputs match exactly
-    f3=/data/"${1}"/TED."${1}"/outputs_nolog.txt
-    find /data/"${1}"/TED."${1}"/* \
-        -exec basename {} \; | grep -v outputs.out | sort > /data/"${1}"/TED."${1}"/outputs.out
-    cat $f2 | grep -v -E -e '^tedana_[12][0-9]{3}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.txt$' > $f3
-    diff $f1 $f3
-}
-
-
-_run_integration_test() {
-    #
-    # Runs tedana integration tests for specified dataset
-    #
-    # Required argments:
-    #   dataset         name of dataset to use for testing. should be one of
-    #                   [three-echo, five-echo]
-
-    if [ -z "${1}" ] || { [ "${1}" != "three-echo" ] && [ "${1}" != "five-echo" ]; }; then
-        printf 'Must supply dataset name for running integration test; ' >&2
-        printf 'must be one of [three-echo, five-echo]\n' >&2
-        return
-    fi
-    ds=${1}
-    cprint "RUNNING INTEGRATION TESTS FOR DATASET: ${ds}"
-    source activate tedana_py36
-    python setup.py -q install
-    py.test tedana/tests/test_integration_${ds/-/_}.py
-    _check_tedana_outputs "${ds}"
-}
-
-
 run_integration_tests() {
     #
-    # Runs tedana integration tests for both three-echo and five-echo datasets
+    # Runs tedana integration tests; passes any parameters to py.test
     #
 
-    for ds in three-echo five-echo; do
-        _run_integration_test ${ds}
-    done
+    cprint "RUNNING INTEGRATION TESTS"
+    source activate tedana_py36
+    python setup.py -q install
+    py.test "$@" tedana/tests/test_integration.py
 }
 
 
@@ -176,7 +109,7 @@ run_unit_tests() {
         cprint "RUNNING UNIT TESTS FOR PYTHON ENVIRONMENT: ${pyenv}"
         source activate ${pyenv}
         python setup.py -q install
-        py.test --ignore-glob=tedana/tests/test_integration*.py tedana
+        py.test --ignore-glob=tedana/tests/test_integration.py tedana
     done
 }
 
@@ -186,7 +119,7 @@ run_lint_tests() {
     # Lints the tedana codebase
     #
 
-    cprint "LINTING TEDANA CODEBASE"
+    cprint "RUNNING FLAKE8 ON TEDANA DIRECTORY"
     source activate tedana_py36
     flake8 tedana
 }
@@ -194,14 +127,20 @@ run_lint_tests() {
 
 run_tests() {
     #
-    # Runs tedana test suite EXCLUDING FIVE-ECHO TEST
+    # Runs tedana test suite excluding five-echo test by default
     #
+
+    if [ ! -z "${1}" ] && [ "${1}" == "--include-five-echo" ]; then
+        run_five_echo='--include-five-echo'
+    fi
 
     run_lint_tests
     run_unit_tests
-    _run_integration_test three-echo
+    run_integration_tests "${run_five_echo}"
 
-    cprint "FINISHED RUNNING TESTS! GREAT SUCCESS"
+    if [ -z "${run_five_echo}" ]; then
+        cprint "FINISHED RUNNING TESTS! GREAT SUCCESS"
+    fi
 }
 
 
@@ -210,9 +149,7 @@ run_all_tests() {
     # Runs entire tedana test suite
     #
 
-    run_lint_tests
-    run_unit_tests
-    run_integration_tests
+    run_tests --include-five-echo
 
     cprint "FINISHED RUNNING ALL TESTS! GREAT SUCCESS"
 }
