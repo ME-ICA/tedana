df6261f50b1ca291638ab7bbd12c822d43d82cd2
Joshua Teves <jbtevespro@gmail.com>
2019-11-06 15:50:31 -0500

    Resolves merge conflict, adds output check

--- tedana/tests/test_integration.py
+++ tedana/tests/test_integration.py
@@ -1,6 +1,31 @@
 import pytest
+import os.path as op
+import re
 from tedana.workflows import tedana_workflow
 
+
+def check_outputs(fname, outpath):
+    """
+    Checks outputs of integration tests
+    """
+    # Checks named files
+    with open(fname, 'r') as f:
+        tocheck = f.read().splitlines()
+
+    for f in tocheck:
+        assert op.exists(f)
+
+    # Checks for log file
+    logfiles = []
+    log_regex = ('^tedana_'
+                 '[12][0-9]{3}-[0-9]{2}-[0-9]{2}T[0-9]{2}:'
+                 '[0-9]{2}:[0-9]{2}.txt$')
+    outputs = os.listdir(outpath)
+    for out in outputs:
+        if re.match(log_regex, out):
+            logfiles.append(out)
+
+    assert len(logfiles) == 1
 
 def test_integration_five_echo(skip_integration, include_five_echo):
     """
@@ -8,11 +33,14 @@ def test_integration_five_echo(skip_integration, include_five_echo):
     """
     if skip_integration or not include_five_echo:
         pytest.skip('Skipping five-echo integration')
+    outputs_file = '/tmp/src/tedana/.circleci/tedana_outputs_verbose.txt'
+    out_dir = '/tmp/data/five-echo/TED.five-echo'
     tedana_workflow(
         data='/data/five-echo/p06.SBJ01_S09_Task11_e[1,2,3,4,5].sm.nii.gz',
         tes=[15.4, 29.7, 44.0, 58.3, 72.6],
-        out_dir='/data/five-echo/TED.five-echo',
+        out_dir=out_dir,
         debug=True, verbose=True)
+    check_outputs(outputs_file, out_dir)
 
 
 def test_integration_three_echo(skip_integration):
@@ -21,8 +49,11 @@ def test_integration_three_echo(skip_integration):
     """
     if skip_integration:
         pytest.skip('Skipping three-echo integration.')
+    outputs_file = '/tmp/src/tedana/.circleci/tedana_outputs.txt'
+    out_dir = '/tmp/data/three-echo/TED.three-echo'
     tedana_workflow(
         data='/data/three-echo/three_echo_Cornell_zcat.nii.gz',
         tes=[14.5, 38.5, 62.5],
-        out_dir='/data/three-echo/TED.three-echo',
+        out_dir=out_dir,
         tedpca='kundu', png=True)
+    check_outputs(outputs_file, out_dir)
