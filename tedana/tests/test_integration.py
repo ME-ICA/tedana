494bc638f152c08e8f4378fec6add51baab32a08
Ross Markello <rossmarkello@gmail.com>
2019-11-06 21:48:05 -0500

    [TEST] Fixes pytest integration path checking

--- tedana/tests/test_integration.py
+++ tedana/tests/test_integration.py
@@ -1,5 +1,9 @@
+"""
+Integration tests for "real" data
+"""
+
 import pytest
-import os.path as op
+import os
 import re
 from tedana.workflows import tedana_workflow
 
@@ -7,40 +11,42 @@ from tedana.workflows import tedana_workflow
 def check_outputs(fname, outpath):
     """
     Checks outputs of integration tests
+
+    Parameters
+    ----------
+    fname : str
+        Path to file with expected outputs
+    outpath : str
+        Path to output directory generated from integration tests
     """
+
     # Checks named files
     with open(fname, 'r') as f:
         tocheck = f.read().splitlines()
-
-    for f in tocheck:
-        assert op.exists(f)
+    for fname in tocheck:
+        assert os.path.exists(os.path.join(outpath, fname))
 
     # Checks for log file
-    logfiles = []
     log_regex = ('^tedana_'
                  '[12][0-9]{3}-[0-9]{2}-[0-9]{2}T[0-9]{2}:'
                  '[0-9]{2}:[0-9]{2}.txt$')
-    outputs = os.listdir(outpath)
-    for out in outputs:
-        if re.match(log_regex, out):
-            logfiles.append(out)
-
+    logfiles = [out for out in os.listdir(outpath) if re.match(log_regex, out)]
     assert len(logfiles) == 1
+
 
 def test_integration_five_echo(skip_integration, include_five_echo):
     """
     An integration test of the full tedana workflow using five-echo test data.
     """
     if skip_integration or not include_five_echo:
-        pytest.skip('Skipping five-echo integration')
-    outputs_file = '/tedana/.circleci/tedana_outputs_verbose.txt'
+        pytest.skip('Skipping five-echo integration test')
     out_dir = '/data/five-echo/TED.five-echo'
-    # tedana_workflow(
-    #     data='/data/five-echo/p06.SBJ01_S09_Task11_e[1,2,3,4,5].sm.nii.gz',
-    #     tes=[15.4, 29.7, 44.0, 58.3, 72.6],
-    #     out_dir=out_dir,
-    #     debug=True, verbose=True)
-    check_outputs(outputs_file, out_dir)
+    tedana_workflow(
+        data='/data/five-echo/p06.SBJ01_S09_Task11_e[1,2,3,4,5].sm.nii.gz',
+        tes=[15.4, 29.7, 44.0, 58.3, 72.6],
+        out_dir=out_dir,
+        debug=True, verbose=True)
+    check_outputs('/tedana/.circleci/tedana_outputs_verbose.txt', out_dir)
 
 
 def test_integration_three_echo(skip_integration):
@@ -48,12 +54,11 @@ def test_integration_three_echo(skip_integration):
     An integration test of the full tedana workflow using three-echo test data.
     """
     if skip_integration:
-        pytest.skip('Skipping three-echo integration.')
-    outputs_file = '/tedana/.circleci/tedana_outputs.txt'
+        pytest.skip('Skipping three-echo integration test')
     out_dir = '/data/three-echo/TED.three-echo'
     tedana_workflow(
         data='/data/three-echo/three_echo_Cornell_zcat.nii.gz',
         tes=[14.5, 38.5, 62.5],
         out_dir=out_dir,
         tedpca='kundu', png=True)
-    check_outputs(outputs_file, out_dir)
+    check_outputs('/tedana/.circleci/tedana_outputs.txt', out_dir)
