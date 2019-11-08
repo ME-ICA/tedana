dc1281c9966d3fe5fee4bffc5bb4b737df8c73fe
Joshua Teves <jbtevespro@gmail.com>
2019-11-08 14:48:56 -0500

    Address @emdupre's nitpck

--- tedana/workflows/tedana.py
+++ tedana/workflows/tedana.py
@@ -148,8 +148,8 @@ def _get_parser():
                                 'Set to -1 for varying results across ICA calls. '
                                 'Default=42.'),
                           default=42)
-    optional.add_argument('--nopng',
-                          dest='nopng',
+    optional.add_argument('--no-png',
+                          dest='no_png',
                           action='store_true',
                           help=('Creates a figures folder with static component '
                                 'maps, timecourse plots and other diagnostic '
@@ -213,7 +213,7 @@ def tedana_workflow(data, tes, mask=None, mixm=None, ctab=None, manacc=None,
                     tedort=False, gscontrol=None, tedpca='mle',
                     source_tes=-1, combmode='t2s', verbose=False, stabilize=False,
                     out_dir='.', fixed_seed=42, maxit=500, maxrestart=10,
-                    debug=False, quiet=False, nopng=False,
+                    debug=False, quiet=False, no_png=False,
                     png_cmap='coolwarm',
                     low_mem=False, fittype='loglin'):
     """
@@ -262,11 +262,11 @@ def tedana_workflow(data, tes, mask=None, mixm=None, ctab=None, manacc=None,
         which is slightly slower but may be more accurate.
     verbose : :obj:`bool`, optional
         Generate intermediate and additional files. Default is False.
-    nopng : obj:'bool', optional
+    no_png : obj:'bool', optional
         Do not generate .png plots and figures. Default is false.
     png_cmap : obj:'str', optional
             Name of a matplotlib colormap to be used when generating figures.
-            Cannot be used with --nopng. Default 'coolwarm'
+            Cannot be used with --no-png. Default 'coolwarm'
     out_dir : :obj:`str`, optional
         Output directory.
 
@@ -376,13 +376,13 @@ def tedana_workflow(data, tes, mask=None, mixm=None, ctab=None, manacc=None,
     n_samp, n_echos, n_vols = catd.shape
     LGR.debug('Resulting data shape: {}'.format(catd.shape))
 
-    if nopng and (png_cmap != 'coolwarm'):
-        LGR.warning('Overriding --nopng since --png-cmap provided.')
-        nopng = False
+    if no_png and (png_cmap != 'coolwarm'):
+        LGR.warning('Overriding --no-png since --png-cmap provided.')
+        no_png = False
 
     # check if TR is 0
     img_t_r = ref_img.header.get_zooms()[-1]
-    if img_t_r == 0 and not nopng:
+    if img_t_r == 0 and not no_png:
         raise IOError('Dataset has a TR of 0. This indicates incorrect'
                       ' header information. To correct this, we recommend'
                       ' using this snippet:'
@@ -542,7 +542,7 @@ def tedana_workflow(data, tes, mask=None, mixm=None, ctab=None, manacc=None,
     if verbose:
         io.writeresults_echoes(catd, mmix, mask, comptable, ref_img)
 
-    if not nopng:
+    if not no_png:
         LGR.info('Making figures folder with static component maps and '
                  'timecourse plots.')
         # make figure folder first
