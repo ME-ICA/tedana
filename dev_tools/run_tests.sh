a17e87295e92781898dca6a5573fe0f2380bfd89
Joshua Teves <jbtevespro@gmail.com>
2019-11-20 11:13:54 -0500

    Fixes run_tests to include 4-echo

--- dev_tools/run_tests.sh
+++ dev_tools/run_tests.sh
@@ -74,6 +74,7 @@ run_all_tests() {
     run_lint_tests
     run_unit_tests
     run_three_echo_test
+    run_four_echo_test
     run_five_echo_test
 
     cprint "FINISHED RUNNING ALL TESTS -- GREAT SUCCESS !"
