162ec92bb786bbeafe7da05c9f7a71d2507f51a6
Taylor Salo <tsalo006@fiu.edu>
2019-12-13 15:56:31 -0500

    Add args to integration tests.

--- tedana/tests/test_integration.py
+++ tedana/tests/test_integration.py
@@ -91,7 +91,10 @@ def test_integration_five_echo(skip_integration):
         tes=[15.4, 29.7, 44.0, 58.3, 72.6],
         out_dir=out_dir,
         tedpca='mdl',
-        debug=True, verbose=True)
+        gscontrol=['gsr', 't1c'],
+        png_cmap='bone',
+        debug=True,
+        verbose=True)
 
     # Just a check on the component table pending a unit test of load_comptable
     comptable = os.path.join(out_dir, 'ica_decomposition.json')
@@ -151,7 +154,16 @@ def test_integration_three_echo(skip_integration):
         tes=[14.5, 38.5, 62.5],
         out_dir=out_dir,
         low_mem=True,
-        tedpca='kundu')
+        tedpca='kundu-stabilize',
+        no_png=True)
+
+    tedana_workflow(
+        data='/tmp/data/three-echo/three_echo_Cornell_zcat.nii.gz',
+        tes=[14.5, 38.5, 62.5],
+        out_dir=out_dir,
+        mix=op.join(out_dir, 'ica_mixing.tsv'),
+        ctab=op.join(out_dir, 'ica_decomposition.json'),
+        no_png=True)
 
     # compare the generated output files
     fn = resource_filename('tedana',
