fb9f54630fff3947ee783ae77ee39be9e210de1d
Taylor Salo <tsalo006@fiu.edu>
2020-02-22 12:34:49 -0500

    Add comment about copy.
    Using copy makes more sense here than re-implementing what I did in
    #498.

--- tedana/metrics/collect.py
+++ tedana/metrics/collect.py
@@ -104,7 +104,7 @@ def generate_metrics(data_cat, data_optcom, mixing, mask, tes, ref_img,
     data_optcom = data_optcom[mask, :]
 
     required_metrics = dependency_resolver(METRIC_DEPENDENCIES, metrics, INPUTS)
-    mixing = mixing.copy()
+    mixing = mixing.copy()  # Use copy to avoid changing the original variable outside of thise function
 
     # Generate the component table, which will be filled out, column by column,
     # throughout this function
