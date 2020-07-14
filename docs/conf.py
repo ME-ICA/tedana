33ac8ffb8eaf4f6d75f0cdd4a41f8eba7c0de015
Taylor Salo <tsalo006@fiu.edu>
2020-07-13 21:22:06 -0400

    Make caution boxes red.
    Also add Napoleon settings.

--- docs/conf.py
+++ docs/conf.py
@@ -101,6 +101,21 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
+# -----------------------------------------------------------------------------
+# Napoleon settings
+# -----------------------------------------------------------------------------
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = True
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = True
+napoleon_use_param = False
+napoleon_use_keyword = True
+napoleon_use_rtype = False
 
 # -- Options for HTML output ----------------------------------------------
 
