82bda7c8de5510ce3534f51760234ebb800f4281
Taylor Salo <tsalo006@fiu.edu>
2021-01-22 17:26:50 -0500

    [ENH] Implement variance explained threshold-based PCA option (#658)
    * Implement varex-based PCA.
    
    * Update documentation as well.
    
    * Fix test.
    
    * Add test for new function.
    
    * Address review.
    
    * Fix problems.

--- tedana/workflows/parser_utils.py
+++ tedana/workflows/parser_utils.py
@@ -3,6 +3,32 @@ Functions for parsers.
 """
 import os.path as op
 import logging
+from numbers import Number
+
+import argparse
+
+
+def check_tedpca_value(string, is_parser=True):
+    """Check if argument is a float in range 0-1 or one of a list of strings."""
+    valid_options = ("mdl", "aic", "kic", "kundu", "kundu-stabilize")
+    msg = None
+    if string not in valid_options:
+        if not isinstance(string, Number):
+            msg = "Argument must be a float or one of: {}".format(
+                ", ".join(valid_options)
+            )
+        elif not (0 <= float(string) <= 1):
+            msg = "Argument must be between 0 and 1."
+        else:
+            string = float(string)
+
+    if msg:
+        if is_parser:
+            raise argparse.ArgumentTypeError(msg)
+        else:
+            raise ValueError(msg)
+
+    return string
 
 
 def is_valid_file(parser, arg):
