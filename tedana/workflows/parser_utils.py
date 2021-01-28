ca7e14521c1b632f46055502bcf19b1d4f4db8f0
Zaki A <notZaki@users.noreply.github.com>
2021-01-28 13:33:24 -0600

    [ENH] Allow tedpca argument to be a float inside a string (#665)
    * Allow tedpca argument to be string containing float
    
    * Appease the style checker
    
    * Add space between lines for readability
    
    * Specify that error message is for tedpca argument

--- tedana/workflows/parser_utils.py
+++ tedana/workflows/parser_utils.py
@@ -3,7 +3,6 @@ Functions for parsers.
 """
 import os.path as op
 import logging
-from numbers import Number
 
 import argparse
 
@@ -11,24 +10,21 @@ import argparse
 def check_tedpca_value(string, is_parser=True):
     """Check if argument is a float in range 0-1 or one of a list of strings."""
     valid_options = ("mdl", "aic", "kic", "kundu", "kundu-stabilize")
-    msg = None
-    if string not in valid_options:
-        if not isinstance(string, Number):
-            msg = "Argument must be a float or one of: {}".format(
-                ", ".join(valid_options)
-            )
-        elif not (0 <= float(string) <= 1):
-            msg = "Argument must be between 0 and 1."
-        else:
-            string = float(string)
+    if string in valid_options:
+        return string
 
-    if msg:
-        if is_parser:
-            raise argparse.ArgumentTypeError(msg)
-        else:
-            raise ValueError(msg)
+    error = argparse.ArgumentTypeError if is_parser else ValueError
+    try:
+        floatarg = float(string)
+    except ValueError:
+        msg = "Argument to tedpca must be a float or one of: {}".format(
+            ", ".join(valid_options)
+        )
+        raise error(msg)
 
-    return string
+    if not (0 <= floatarg <= 1):
+        raise error("Float argument to tedpca must be between 0 and 1.")
+    return floatarg
 
 
 def is_valid_file(parser, arg):
