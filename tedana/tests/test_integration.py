93a917c0f611011c3492f3531c14ca587c4aa499
Zaki A <zaki@live.ca>
2021-02-03 16:46:50 -0600

    Use platform-agonostic paths in integration tests

--- tedana/tests/test_integration.py
+++ tedana/tests/test_integration.py
@@ -49,6 +49,7 @@ def check_integration_outputs(fname, outpath):
     # Compares remaining files with those expected
     with open(fname, 'r') as f:
         tocheck = f.read().splitlines()
+    tocheck = [os.path.normpath(path) for path in tocheck]
     assert sorted(tocheck) == sorted(existing)
 
 
