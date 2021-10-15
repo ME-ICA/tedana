ed2d7109e2108865a4141d068db73c2e988527d9
Joshua Teves <joshua.teves@nih.gov>
2021-10-15 09:42:36 -0400

    Add 3.10 unit test, compatibilities in info.py

--- tedana/info.py
+++ tedana/info.py
@@ -79,4 +79,5 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
 ]
