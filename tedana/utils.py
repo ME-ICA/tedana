Fix the masking.

tedana/utils.py
@@ -100,11 +100,10 @@ def make_adaptive_mask(data, mask=None, getsum=False, threshold=1, ignore_zeros=
     else:
         # if the user has supplied a binary mask
         mask = reshape_niimg(mask).astype(bool)
+        masksum = masksum * mask
         if ignore_zeros:
             # Use value of 1 when mask include voxel but masksum says it's all bad data
             masksum = np.maximum(masksum, mask, dtype=int)
-        else:
-            masksum = masksum * mask
 
         # reduce mask based on masksum
         # TODO: Use visual report to make checking the reduced mask easier