4325c32458d6c1c1129ebde25dfb63963d13b597
Joshua Teves <joshua.teves@nih.gov>
2022-12-22 17:47:46 -0500

    Maybe silence duecredit and re-trigger Circle

--- tedana/__init__.py
+++ tedana/__init__.py
@@ -13,5 +13,6 @@ __version__ = get_versions()["version"]
 
 # cmp is not used, so ignore nipype-generated warnings
 warnings.filterwarnings("ignore", r"cmp not installed")
+warnings.filterwarnings("ignore", r"Failed to import duecredit due to No module named 'duecredit'")
 
 del get_versions
