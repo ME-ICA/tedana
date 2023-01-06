24e3f73773b9b0719a20fcd97b4ddffffb040b66
Joshua Teves <joshua.teves@nih.gov>
2023-01-06 14:17:01 -0500

    Adds bibtex warning check test

---
+++ tedana/tests/test_bibtex.py
@@ -0,0 +1,13 @@
+"""Tests for bibtex"""
+
+import logging
+
+import pytest
+
+from tedana import bibtex
+
+def test_warn_no_citation_found(caplog):
+    citations = ["Nonexistent et al, 0 AD"]
+    ref_list = []
+    bibtex.reduce_references(citations, ref_list)
+    assert f"Citation {citations[0]} not found." in caplog.text
