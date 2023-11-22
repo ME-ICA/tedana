385558cd9ab8d9e208beace6f6856c65b7817475
Marta Arbizu Gómez <127724722+martaarbizu@users.noreply.github.com>
2023-11-22 17:33:11 +0100

    Update io unit test

--- tedana/tests/test_io.py
+++ tedana/tests/test_io.py
@@ -142,12 +142,25 @@ def test_smoke_write_split_ts():
     classification = np.random.choice(["accepted", "rejected", "ignored"], n_components)
     df_data = np.column_stack((component, metric, classification))
     comptable = pd.DataFrame(df_data, columns=["component", "metric", "classification"])
+    io_generator.verbose = True
 
     me.write_split_ts(data, mmix, mask, comptable, io_generator)
 
     # TODO: midk_ts.nii is never generated?
     fn = io_generator.get_name
     split = ("high kappa ts img", "low kappa ts img", "denoised ts img")
+    fnames = [fn(f) for f in split]
+    for filename in fnames:
+        # remove all files generated
+        os.remove(filename)
+
+    io_generator.verbose = False
+    
+    me.write_split_ts(data, mmix, mask, comptable, io_generator)
+
+    # TODO: midk_ts.nii is never generated?
+    fn = io_generator.get_name
+    split = ("denoised ts img")
     fnames = [fn(f) for f in split]
     for filename in fnames:
         # remove all files generated
