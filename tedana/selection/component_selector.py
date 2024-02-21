11c03fb9c6741ad3c1215061c82afabc417da23e
Taylor Salo <tsalo006@fiu.edu>
2024-02-21 08:49:23 -0500

    Include generated metrics in necessary metrics.

--- tedana/selection/component_selector.py
+++ tedana/selection/component_selector.py
@@ -453,8 +453,9 @@ class ComponentSelector:
 
         If either of these happen, a warning is added to the logger.
         """
-        not_declared = self.tree["used_metrics"] - set(self.necessary_metrics)
-        not_used = set(self.necessary_metrics) - self.tree["used_metrics"]
+        necessary_metrics = self.necessary_metrics.union(self.tree["generated_metrics"])
+        not_declared = self.tree["used_metrics"] - set(necessary_metrics)
+        not_used = set(necessary_metrics) - self.tree["used_metrics"]
         if len(not_declared) > 0:
             LGR.warning(
                 f"Decision tree {self.tree_name} used the following metrics that were "
