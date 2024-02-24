123c91603ce15e1189963ead44743ceb0f044360
Taylor Salo <tsalo006@fiu.edu>
2024-02-24 11:40:42 -0500

    Output results in tedana.

--- tedana/workflows/tedana.py
+++ tedana/workflows/tedana.py
@@ -626,6 +626,21 @@ def tedana_workflow(
             io_generator.save_file(utils.millisec2sec(t2s_limited), "limited t2star img")
             io_generator.save_file(s0_limited, "limited s0 img")
 
+    rmse_map, rmse_timeseries, rmse_sd_timeseries = decay.model_fit_decay_ts(
+        data=catd,
+        tes=tes,
+        adaptive_mask=masksum_denoise,
+        t2s=t2s_limited,
+        s0=s0_limited,
+        fitmode="all",
+    )
+    confounds_df = pd.DataFrame(
+        columns=["RMSE", "RMSE_stdev"],
+        data=np.column_stack((rmse_timeseries, rmse_sd_timeseries)),
+    )
+    io_generator.save_file(rmse_map, "rmse img")
+    io_generator.save_file(confounds_df, "confounds tsv")
+
     # optimally combine data
     data_oc = combine.make_optcom(catd, tes, masksum_denoise, t2s=t2s_full, combmode=combmode)
 
