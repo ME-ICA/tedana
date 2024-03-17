72da98ea71d0d381b1fa131c34ec4a8bb2e081a5
Taylor Salo <salot@pennmedicine.upenn.edu>
2024-03-17 10:52:27 -0400

    Update tedana/selection/selection_utils.py

--- tedana/selection/selection_utils.py
+++ tedana/selection/selection_utils.py
@@ -332,7 +332,7 @@ def confirm_metrics_exist(component_table, necessary_metrics, function_name=None
         If ``metrics_exist`` is False then raise an error and end the program.
 
     Notes
-    ----
+    -----
     This doesn't check if there are data in each metric's column, just that the columns exist.
     Also, the string in ``necessary_metrics`` and the column labels in ``component_table`` will
     only be matched if they're identical.
