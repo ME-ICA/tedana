e59795388d3de6d0749f225d64999a966553ec50
Eneko Uruñuela <e.urunuela@icloud.com>
2025-04-28 15:28:06 -0600

    Move r_ica defaults outside of mixing_file conditional

--- tedana/workflows/tedana.py
+++ tedana/workflows/tedana.py
@@ -806,6 +806,10 @@ def tedana_workflow(
     fout = io_generator.save_file(data_optcom, "combined img")
     LGR.info(f"Writing optimally combined data set: {fout}")
 
+    # Default r_ica results to None as they are expected for the reports
+    cluster_labels = None
+    similarity_t_sne = None
+
     if mixing_file is None:
         # Identify and remove thermal noise from data
         data_reduced, n_components = decomposition.tedpca(
@@ -829,8 +833,6 @@ def tedana_workflow(
         keep_restarting = True
         n_restarts = 0
         seed = fixed_seed
-        cluster_labels = None
-        similarity_t_sne = None
 
         while keep_restarting:
             mixing, seed, cluster_labels, similarity_t_sne = decomposition.tedica(
